f29fb497aaf6d5eafdecf34db29e2ca014b1b903
ibaitxo <ibai1989@gmail.com>
2021-09-07 18:31:35 +0000

    Comentando código

--- elk/start-elk.sh
+++ elk/start-elk.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 ufw disable
-
-#/usr/share/filebeat/bin/filebeat -e -c /etc/filebeat/filebeat.yml
-#/usr/share/logstash/bin/logstash --path.settings /etc/logstash/ -f /etc/logstash/conf.d/logstash.conf
+filebeat -e
 
