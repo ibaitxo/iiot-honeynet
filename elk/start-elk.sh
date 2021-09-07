e706f3a513199a1715263f2ea08921cf0ff76503
ibaitxo <ibai1989@gmail.com>
2021-09-07 17:27:41 +0000

    Trabajando en los logs

--- elk/start-elk.sh
+++ elk/start-elk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ufw disable
-#-f /etc/logstash/conf.d/logstash.conf
-#/usr/share/logstash/bin/logstash --path.settings /etc/logstash/ -f /etc/logstash/conf.d/logstash.conf
+
 #/usr/share/filebeat/bin/filebeat -e -c /etc/filebeat/filebeat.yml
+#/usr/share/logstash/bin/logstash --path.settings /etc/logstash/ -f /etc/logstash/conf.d/logstash.conf
+
